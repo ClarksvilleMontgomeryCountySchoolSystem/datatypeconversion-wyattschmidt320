a7cd36e1cdf4d50367a4f12804f0a26fa5209b34
Wyatt Schmidt <wyatt.schmidt320@students.cmcss.net>
2026-02-23 11:49:29 -0600

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,1 +1,44 @@
+from data import a, b, c, d, e, f
+
+# Convert each variable to the most suitable data type
+# Hint: print and check type if unsure
+# Not all can or should be converted to a number
+
+# a
+print(a)
+print(type(a))
+a = float(a)
+print(a)
+print(type(a))
+
+# b
+print(b)
+print(type(b))
+
+
+# c
+print(c)
+print(type(c))
+c = int(c)
+print(d)
+print(type(d))
+# d
+print(d)
+print(type(d))
+d = int(d)
+print(d)
+print(type(d))
+
+# e
+print(e)
+print(type(e))
+
+
+# f
+print(f)
+print(type(f))
+f = float(f)
+print(f)
+print(type(f))
+
 
